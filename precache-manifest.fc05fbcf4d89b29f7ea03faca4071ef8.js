e479ddaa80d1070086bb3c9f0d8f513738ee9a71
shagunsharma6677 <shagunsharma6677@gmail.com>
2023-02-28 01:54:35 +0530

    Updates

---
+++ precache-manifest.fc05fbcf4d89b29f7ea03faca4071ef8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "0b648c625fa2da80a2e7",
+    "url": "/static/css/main.86aa7109.chunk.css"
+  },
+  {
+    "revision": "0b648c625fa2da80a2e7",
+    "url": "/static/js/main.0b648c62.chunk.js"
+  },
+  {
+    "revision": "f09d6da400d0c14a7f1d",
+    "url": "/static/js/1.f09d6da4.chunk.js"
+  },
+  {
+    "revision": "cddc92c48fb4db0237a8",
+    "url": "/static/js/2.cddc92c4.chunk.js"
+  },
+  {
+    "revision": "0e85d9ea22f618c5f874",
+    "url": "/static/js/runtime~main.0e85d9ea.js"
+  },
+  {
+    "revision": "3582131820f416250d1948971aeccde4",
+    "url": "/index.html"
+  }
+];
